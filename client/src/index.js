49df29528bc710d8441108fe432e0a5ff1daa7ca
Gamspi <niko.tatt007@gmail.com>
2022-08-02 17:12:27 +0700

    add client

---
+++ client/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Core from './modules/core/Core';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Core />
+  </React.StrictMode>
+);
---
+++ client/src/modules/core/Core.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Core.css';
+import {} from '@apollo/client'
+
+function Core() {
+  return (
+    <div className="App">
+      working
+    </div>
+  );
+}
+
+export default Core;
